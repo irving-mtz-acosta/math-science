513a75da180411c30cafa823c77dc2580d123f3e
irving-mtz-acosta <irving915656@gmail.com>
2026-01-23 16:59:53 -0700

    Update website

--- update_and_publish.sh
+++ update_and_publish.sh
@@ -3,8 +3,7 @@ set -e #stop if any command fails
 
 git add .
 git commit -m "Update website"
-git pull origin main    
-git pull origin main    
+git push origin main    
 
 cd my-site
 quarto publish gh-pages --no-prompt
